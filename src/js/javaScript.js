4775aad8547c252810da6619a9b2fcb6407fd5a4
Lucas  Souza <souza.lucasp10@gmail.com>
2023-06-01 18:22:49 -0300

    continuando o portfolio

--- src/js/javaScript.js
+++ src/js/javaScript.js
@@ -23,7 +23,7 @@ const sr = ScrollReveal ({
     reset: true
 })
 
-sr.reveal('.home-text',{delay:350, origin:'left'})
-sr.reveal('.home-img',{delay:350, origin:'right'})
+sr.reveal('.home-text',{delay:150, origin:'left'})
+sr.reveal('.home-img',{delay:150, origin:'right'})
 
-sr.reveal('.sub-service,.about,.portifolio,.contact',{delay:200, origin:'bottom'})
+sr.reveal('.sub-service,.about,.portifolio,.contact',{delay:50, origin:'bottom'})
